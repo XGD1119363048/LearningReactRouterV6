9d3639c8bac6b23092ef22d6881299678c468ac4
许国栋(Guodong Xu) <gdxu@univista-isg.com>
2023-12-12 16:31:43 +0800

    嵌套路由

--- src/router/index.js
+++ src/router/index.js
@@ -5,15 +5,25 @@ import Redirect from '../components/Redirect'
 import Center from '../views/Center'
 import Cinemas from '../views/Cinemas'
 import Films from '../views/Films'
+import ComingSoon from '../views/films/ComingSoon'
+import NowPlaying from '../views/films/NowPlaying'
 import NotFound from '../views/NotFound'
+import Search from '../views/Search'
 
 export default function MRouter() {
   return (
     <Routes>
       {/* <Route path='/' element={<Films />} /> */}
       {/* <Route index element={<Films />} /> */}
-      <Route path='/films' element={<Films />} />
+      <Route path='/films' element={<Films />}>
+        {/* <Route index element={<NowPlaying />} /> */}
+        <Route index element={<Redirect to='/films/nowplaying' />} />
+        <Route path='nowPlaying' element={<NowPlaying />} />
+        <Route path='comingSoon' element={<ComingSoon />} />
+      </Route>
+
       <Route path='/cinemas' element={<Cinemas />} />
+      <Route path='/cinemas/search' element={<Search />} />
       <Route path='/center' element={<Center />} />
 
       {/* 重定向方案1：使用 Navigate 组件 */}
@@ -21,7 +31,7 @@ export default function MRouter() {
       
       {/* 重定向方案2：自定义 Redirect 组件 */}
       <Route path='/' element={<Redirect to='/films' />} />
-      
+
       <Route path='*' element={<NotFound />} />
     </Routes>
   )
--- src/views/Films.js
+++ src/views/Films.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export default function Films() {
   return (
-    <div>Films</div>
+    <div>
+      <div style={{
+        height: '200px',
+        background: 'yellow'
+      }}>大轮播</div>
+
+      {/* 路由容器 */}
+      <Outlet></Outlet>
+    </div>
   )
 }
